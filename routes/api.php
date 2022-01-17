92652d939e72e32baf3090c68f2dc2becd43886f
BIANOX07 <briangd98@gmail.com>
2022-01-17 08:21:14 +0100

    Solución a los ejercicios BRIAN

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,28 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\API\CentroController;
+use App\Http\Controllers\API\NivelController;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Tqdev\PhpCrudApi\Api;
+use Tqdev\PhpCrudApi\Config;
+
+Route::apiResource('centros', CentroController::class);
+Route::apiResource('niveles', NivelController::class)->parameters(['niveles' =>'nivel']);
+
+Route::any('/{any}', function (ServerRequestInterface $request) {
+    $config = new Config([
+        'address' => env('DB_HOST', '127.0.0.1'),
+        'database' => env('DB_DATABASE', 'forge'),
+        'username' => env('DB_USERNAME', 'forge'),
+        'password' => env('DB_PASSWORD', ''),
+        'basePath' => '/api',
+    ]);
+    $api = new Api($config);
+    $response = $api->handle($request);
+    return $response;
+})->where('any', '.*');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
